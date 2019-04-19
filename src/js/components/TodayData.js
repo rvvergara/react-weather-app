aa97985d5e90bf616d815d830f248bb109a4a9d4
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-19 09:51:44 +0800

    Style TodayData

--- src/js/components/TodayData.js
+++ src/js/components/TodayData.js
@@ -12,28 +12,36 @@ const TodayData = (props) => {
   const {
     data,
     celsius,
+    city,
     changeTempUnit
   } = props;
 
   const unitDisp = celsius ? 'C' : 'F';
   const temp = tempToDisplay(unitDisp, data.the_temp);
   return (
-    <div>
-      <h3>{parseDate(data.applicable_date, true)}</h3>
-      <img
-        src={`${weatherImageUrl}${data.weather_state_abbr}.png`} 
-        alt="weather today"
-        className="weather-img"
-        />
-        <TempSelector 
-          celsius={celsius}
-          changeTempUnit={changeTempUnit}
-        />
-        <h3>{temp}&deg;{unitDisp}</h3>
-        <DataTable 
-          data={data}
-          celsius={celsius}
-        />
+    <div id="today-data">
+      <div id="today-data-main">
+        <h1>{city}</h1>
+        <p>{parseDate(data.applicable_date, true)}</p>
+        <div className="weather-img-temp">
+          <img
+            src={`${weatherImageUrl}${data.weather_state_abbr}.png`} 
+            alt="weather today"
+            className="weather-img"
+            />
+            <TempSelector 
+              celsius={celsius}
+              changeTempUnit={changeTempUnit}
+            />
+            <h3>{temp}&deg;{unitDisp}</h3>
+        </div>
+      </div>
+        <div id="today-data-table">
+          <DataTable 
+            data={data}
+            celsius={celsius}
+          />
+        </div>
     </div>
   )
 };
